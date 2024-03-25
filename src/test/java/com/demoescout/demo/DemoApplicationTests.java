d991d93a922b146788ecac7162e63fb9e7c559c7
beyoncerii <2021614578@student.uitm.edu.my>
2024-03-25 16:51:16 +0800

    first commit

---
+++ src/test/java/com/demoescout/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoescout.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
